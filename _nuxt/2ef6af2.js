04d5c6b8833a860b8991c1168c91791a4dd42ef8
beamjyp <68525436+beamjyp@users.noreply.github.com>
2020-10-02 23:57:12 +0700

    Add files via upload

---
+++ _nuxt/2ef6af2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{283:function(e,n,t){"use strict";t.r(n);var l={},c=t(73),component=Object(c.a)(l,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("defult")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
